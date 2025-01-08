db989bcb30a36179d05280bf0761daf466713ee4
Erwan DELAPORTE <edelapor@sio.lan>
2025-01-08 16:25:21 +0100

    style dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4,13 +4,35 @@
         flex-direction : column;
         justify-content : center;
         align-items : center;
-        gap : 10px;
+        height : 60%;
+        gap : 10%;
+        padding : 20px;
     }
-    a{
-        width : 10%;
+    .a_nav{
+        padding-top : 10px;
+        padding-bottom : 10px;
+        border-radius : 10px;
+        color : white;
+        font-size : 14px;
+        width : 75%;
+        text-align : center;
+        background-color : #337ab7; 
+        border-color : #2e6da4 0.5px solid;
+
+    }
+    .a_nav:hover{
+        padding-top : 10px;
+        padding-bottom : 10px;
+        border-radius : 10px;
+        color : white;
+        font-size : 14px;
+        width : 75%;
+        text-align : center;
+        background-color : #2e6da4; 
+        border-color : #2e6da4 0.5px solid;
+        
     }
 </style>
-
 <x-app-layout>
 
 
@@ -18,10 +40,10 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
                 <div class="container-link">
-                    <a href="/clients">Clients</a>
-                    <a href="/rdv">Rendez vous</a>
-                    <a href="/produits">Produits</a>
-                    <a href="/factures">factures</a>
+                    <a class="a_nav" href="/clients">Clients</a>
+                    <a class="a_nav" href="/rdv">Rendez vous</a>
+                    <a class="a_nav" href="/produits">Produits</a>
+                    <a class="a_nav" href="/factures">factures</a>
                     
                 </div>
             </div>
