908cd3f1a278b680d1ba82009d4235fe0c18df08
erwan749 <erwandelaporte71210@gmail.com>
2025-03-25 19:46:50 +0100

    update user api login

--- app/Http/Controllers/CommercialApiController.php
+++ app/Http/Controllers/CommercialApiController.php
@@ -88,6 +88,7 @@ class CommercialApiController extends Controller
         return response()->json([
             "success" => true,
             "data" => [
+                "id" => $user->id,
                 "name" => $user->name,
                 "token" => $token
             ]
