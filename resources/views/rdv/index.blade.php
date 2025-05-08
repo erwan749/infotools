cc15cd2ee90d19f51d1783f98c6912cf8e574617
erwan749 <101586302+erwan749@users.noreply.github.com>
2025-05-08 15:36:48 +0200

    add error display for rdv index

--- resources/views/rdv/index.blade.php
+++ resources/views/rdv/index.blade.php
@@ -13,7 +13,11 @@
             <p>{{ $message }}</p>
         </div>
     @endif
-
+    @if ($message = Session::get('error'))
+        <div class="alert alert-danger">
+            <p>{{ $message }}</p>
+        </div>
+    @endif
     <table class="table table-bordered">
         <thead>
             <tr>
